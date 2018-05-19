339d3ff9188d58a238460f65713b5b406788d5e0
usford <39438766+usford@users.noreply.github.com>
2018-05-19 18:03:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function onetext(){
+	var one = document.getElementById("one");
+	var one1 = window.getComputedStyle(one);
+	if (one1.color == 'rgb(255, 255, 255)'){
+		one.style.color = 'rgb(0, 0, 0)';
+	} 
+	document.getElementById("one").innerHTML="Раз"
+}
+
+function twotext(){
+	var one = document.getElementById("one");
+	var one1 = window.getComputedStyle(one);
+	if (one1.color == 'rgb(255, 255, 255)'){
+		one.style.color = 'rgb(0, 0, 0)';
+	} 
+	document.getElementById("one").innerHTML="Два"
+}
+
+function threetext(){
+	var one = document.getElementById("one");
+	var one1 = window.getComputedStyle(one);
+	if (one1.color == 'rgb(255, 255, 255)'){
+		one.style.color = 'rgb(0, 0, 0)';
+	} 
+	document.getElementById("one").innerHTML="Три"
+}
+
+function fourtext(){
+	var one = document.getElementById("one");
+	var one1 = window.getComputedStyle(one);
+	if (one1.color == 'rgb(255, 255, 255)'){
+		one.style.color = 'rgb(0, 0, 0)';
+	} 
+	document.getElementById("one").innerHTML="Четыре"
+}
